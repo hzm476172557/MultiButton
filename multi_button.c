b3c86d316363beed3bd2025536e6ef2b9935f4c4
ChrisHe <ChrisHe.VIP@hotmail.com>
2019-10-16 11:08:29 +0800

    1. 修改REPEAT模式，修复原来的REPEAT模式工作不正常的情况 2. 添加combine模式(未完成)

--- multi_button.c
+++ multi_button.c
@@ -5,25 +5,86 @@
 
 #include "multi_button.h"
 
-#define EVENT_CB(ev)   if(handle->cb[ev])handle->cb[ev]((Button*)handle)
-	
+#define EVENT_CB(ev)        if(handle->cb[ev]) handle->cb[ev]((Button*)handle)
+#define COMBINE_EVENT_CB()  if(combine_callback) combine_callback(combine_counter)
+
 //button handle list head.
-static struct Button* head_handle = NULL;
+static struct Button *head_handle = NULL;
+
+// calculate the number of combine keys now.
+static uint32_t combine_counter = 0;
+static int32_t combine_button_id_list[COMBINE_MAX_KEY];
+// combine callback function
+static BtnCallback combine_callback = NULL;
+
+static int _button_combine_add(uint16_t button_id) {
+    int result = -1;
+
+    for(uint16_t i = 0; i < COMBINE_MAX_KEY; i++) {
+        if(combine_button_id_list[i] == button_id) {
+            return 0;   // already added.
+        }
+    }
+
+    for(uint16_t i = 0; i < COMBINE_MAX_KEY; i++) {
+        if(combine_button_id_list[i] == COMBINE_BTN_ID_NONE) {
+            combine_button_id_list[i] = button_id;
+            result = 0;
+            break;
+        }
+    }
+
+    return result;
+}
+
+static int _button_combine_del(uint16_t button_id) {
+    int result = -1;
+
+    for(uint16_t i = 0; i < COMBINE_MAX_KEY; i++) {
+        if(combine_button_id_list[i] == button_id) {
+            combine_button_id_list[i] = COMBINE_BTN_ID_NONE;
+            result = 0;
+            break;
+        }
+    }
+
+    return result;
+}
+
+/**
+  * @brief  Check if the specified button_id is in Combine Keys.Used when CombineEvent occurred.
+  * @param  button_id: the specified button id to check.
+  * @retval 0 : not in CombineKeys. others : in CombineKeys
+  */
+uint8_t button_combine_check(uint16_t button_id) {
+    uint8_t result = 0;
+
+    for(uint16_t i = 0; i < COMBINE_MAX_KEY; i++) {
+        if(combine_button_id_list[i] == button_id) {
+            result = 1;
+            break;
+        }
+    }
+
+    return result;
+}
+
 
 /**
   * @brief  Initializes the button struct handle.
   * @param  handle: the button handle strcut.
   * @param  pin_level: read the HAL GPIO of the connet button level.
   * @param  active_level: pressed GPIO level.
+  * @param  button_id:The unique id for this button
   * @retval None
   */
-void button_init(struct Button* handle, uint8_t(*pin_level)(), uint8_t active_level)
-{
-	memset(handle, 0, sizeof(struct Button));
-	handle->event = (uint8_t)NONE_PRESS;
-	handle->hal_button_Level = pin_level;
-	handle->button_level = handle->hal_button_Level();
-	handle->active_level = active_level;
+void button_init(struct Button *handle, uint8_t(*pin_level)(void *btn), uint8_t active_level, uint16_t button_id) {
+    memset(handle, 0, sizeof(struct Button));
+    handle->button_id = button_id;
+    handle->event = (uint8_t)NONE_PRESS;
+    handle->hal_button_Level = pin_level;
+    handle->button_level = handle->hal_button_Level((void *)handle);
+    handle->active_level = active_level;
 }
 
 /**
@@ -33,19 +94,33 @@ void button_init(struct Button* handle, uint8_t(*pin_level)(), uint8_t active_le
   * @param  cb: callback function.
   * @retval None
   */
-void button_attach(struct Button* handle, PressEvent event, BtnCallback cb)
-{
-	handle->cb[event] = cb;
+void button_attach(struct Button *handle, PressEvent event, BtnCallback cb) {
+    if(event < number_of_event) {
+        handle->cb[event] = cb;
+    }
 }
+
+/**
+  * @brief  Attach the combine key event callback function.
+  * @param  cb: callback function.
+  * @retval None
+  */
+void button_combine_event_attach(BtnCallback cb) {
+    combine_callback = cb;
+}
+
 
 /**
   * @brief  Inquire the button event happen.
   * @param  handle: the button handle strcut.
   * @retval button event.
   */
-PressEvent get_button_event(struct Button* handle)
-{
-	return (PressEvent)(handle->event);
+PressEvent get_button_event(struct Button *handle) {
+    return (PressEvent)(handle->event);
+}
+
+uint8_t get_button_repeat_count(struct Button *handle) {
+    return handle->repeat;
 }
 
 /**
@@ -53,100 +128,165 @@ PressEvent get_button_event(struct Button* handle)
   * @param  handle: the button handle strcut.
   * @retval None
   */
-void button_handler(struct Button* handle)
-{
-	uint8_t read_gpio_level = handle->hal_button_Level();
+void button_handler(struct Button *handle) {
+    uint8_t read_gpio_level = handle->hal_button_Level((void *)handle);
 
-	//ticks counter working..
-	if((handle->state) > 0) handle->ticks++;
+    //ticks counter working..
+    if((handle->state) > 0) {
+        handle->ticks++;
+    }
 
-	/*------------button debounce handle---------------*/
-	if(read_gpio_level != handle->button_level) { //not equal to prev one
-		//continue read 3 times same new level change
-		if(++(handle->debounce_cnt) >= DEBOUNCE_TICKS) {
-			handle->button_level = read_gpio_level;
-			handle->debounce_cnt = 0;
-		}
-	} else { //leved not change ,counter reset.
-		handle->debounce_cnt = 0;
-	}
+    /*------------button debounce handle---------------*/
+    if(read_gpio_level != handle->button_level) { //not equal to prev one
+        //continue read 3 times same new level change
+        if(++(handle->debounce_cnt) >= DEBOUNCE_TICKS) {
+            handle->button_level = read_gpio_level;
+            handle->debounce_cnt = 0;
+        }
+    } else { //leved not change ,counter reset.
+        handle->debounce_cnt = 0;
+    }
 
-	/*-----------------State machine-------------------*/
-	switch (handle->state) {
-	case 0:
-		if(handle->button_level == handle->active_level) {	//start press down
-			handle->event = (uint8_t)PRESS_DOWN;
-			EVENT_CB(PRESS_DOWN);
-			handle->ticks = 0;
-			handle->repeat = 1;
-			handle->state = 1;
-		} else {
-			handle->event = (uint8_t)NONE_PRESS;
-		}
-		break;
+    if(handle->event != NONE_PRESS) {
+        _button_combine_add(handle->button_id);
+    }
 
-	case 1:
-		if(handle->button_level != handle->active_level) { //released press up
-			handle->event = (uint8_t)PRESS_UP;
-			EVENT_CB(PRESS_UP);
-			handle->ticks = 0;
-			handle->state = 2;
+    /*-----------------State machine-------------------*/
+    switch(handle->state) {
+        case 0:
+            if(handle->button_level == handle->active_level) {      //start press down
+                combine_counter++;
+                handle->event = (uint8_t)PRESS_DOWN;
+                EVENT_CB(PRESS_DOWN);
+                handle->ticks = 0;
+                handle->repeat = 1;
+                handle->state = 1;
+            } else {
+                if(handle->event != (uint8_t)NONE_PRESS) {
+                    handle->event = (uint8_t)NONE_PRESS;
+                    handle->ticks = 0;
+                    combine_counter--;
+                }
 
-		} else if(handle->ticks > LONG_TICKS) {
-			handle->event = (uint8_t)LONG_RRESS_START;
-			EVENT_CB(LONG_RRESS_START);
-			handle->state = 5;
-		}
-		break;
+                _button_combine_del(handle->button_id);
+            }
 
-	case 2:
-		if(handle->button_level == handle->active_level) { //press down again
-			handle->event = (uint8_t)PRESS_DOWN;
-			EVENT_CB(PRESS_DOWN);
-			handle->repeat++;
-			if(handle->repeat == 2) {
-				EVENT_CB(DOUBLE_CLICK); // repeat hit
-			} 
-			EVENT_CB(PRESS_REPEAT); // repeat hit
-			handle->ticks = 0;
-			handle->state = 3;
-		} else if(handle->ticks > SHORT_TICKS) { //released timeout
-			if(handle->repeat == 1) {
-				handle->event = (uint8_t)SINGLE_CLICK;
-				EVENT_CB(SINGLE_CLICK);
-			} else if(handle->repeat == 2) {
-				handle->event = (uint8_t)DOUBLE_CLICK;
-			}
-			handle->state = 0;
-		}
-		break;
+            break;
 
-	case 3:
-		if(handle->button_level != handle->active_level) { //released press up
-			handle->event = (uint8_t)PRESS_UP;
-			EVENT_CB(PRESS_UP);
-			if(handle->ticks < SHORT_TICKS) {
-				handle->ticks = 0;
-				handle->state = 2; //repeat press
-			} else {
-				handle->state = 0;
-			}
-		}
-		break;
+        case 1:
+            if(handle->button_level != handle->active_level) {      //released press up
+                handle->event = (uint8_t)PRESS_UP;
+                EVENT_CB(PRESS_UP);
 
-	case 5:
-		if(handle->button_level == handle->active_level) {
-			//continue hold trigger
-			handle->event = (uint8_t)LONG_PRESS_HOLD;
-			EVENT_CB(LONG_PRESS_HOLD);
+                if(handle->ticks >= SHORT_TICKS) {
+                    // SHORT_TICKS has been occurred, then state 2 to wait REPEAT_TICKS
+                    handle->event = (uint8_t)SINGLE_CLICK;
+                    handle->ticks = 0;
+                    handle->state = 2;
+                } else {
+                    // SHORT_TICKS has not been occurred, check Repeat mode.
+                    handle->ticks = 0;
+                    handle->state = 2;
+                }
+            } else if(handle->ticks > LONG_TICKS) {
+                handle->event = (uint8_t)LONG_RRESS_START;
+                EVENT_CB(LONG_RRESS_START);
+                handle->ticks = 0;
+                handle->state = 6;
+            }
 
-		} else { //releasd
-			handle->event = (uint8_t)PRESS_UP;
-			EVENT_CB(PRESS_UP);
-			handle->state = 0; //reset
-		}
-		break;
-	}
+            break;
+
+        case 2:
+            if(handle->button_level == handle->active_level) { //press down again
+                handle->event = (uint8_t)PRESS_DOWN;
+                EVENT_CB(PRESS_DOWN);
+                handle->repeat++;
+                handle->ticks = 0;
+                handle->state = 3;
+            } else {
+                if(handle->ticks > REPEAT_TICKS) { //released timeout, back to state 0
+                    if(handle->repeat == 1) {
+                        if(handle->event == SINGLE_CLICK) {
+                            EVENT_CB(SINGLE_CLICK);
+                        }
+                    } else if(handle->repeat == 2) {
+                        handle->event = (uint8_t)DOUBLE_CLICK;
+                        EVENT_CB(DOUBLE_CLICK); // repeat hit
+                    } else {
+                        handle->event = (uint8_t)PRESS_REPEAT;
+                        EVENT_CB(PRESS_REPEAT); // repeat hit
+                    }
+
+                    handle->state = 0;
+                }
+            }
+
+            break;
+
+        case 3:
+            if(handle->button_level != handle->active_level) {
+                handle->event = (uint8_t)PRESS_UP;
+                EVENT_CB(PRESS_UP);
+
+                handle->ticks = 0;
+                handle->state = 2;
+            } else {
+                if(handle->ticks > LONG_TICKS) {
+                    handle->repeat-- ;  // Detected long press, ignore the last repeat.
+                    if(handle->repeat == 2) {
+                        handle->event = (uint8_t)DOUBLE_CLICK;
+                        EVENT_CB(DOUBLE_CLICK); // repeat hit
+                    } else {
+                        handle->event = (uint8_t)PRESS_REPEAT;
+                        EVENT_CB(PRESS_REPEAT); // repeat hit
+                    }
+
+                    handle->event = (uint8_t)LONG_RRESS_START;
+                    EVENT_CB(LONG_RRESS_START);
+                    handle->ticks = 0;
+                    handle->state = 6;
+                }
+            }
+
+            break;
+
+        case 5:
+            if(handle->button_level == handle->active_level) {
+                //continue hold trigger
+                if(handle->ticks > HOLD_PERIOD_TICKS) {
+                    handle->event = (uint8_t)LONG_PRESS_HOLD;
+                    EVENT_CB(LONG_PRESS_HOLD);
+                    handle->ticks = 0;
+                }
+
+            } else { //releasd
+                handle->event = (uint8_t)PRESS_UP;
+                EVENT_CB(PRESS_UP);
+                handle->state = 0; //reset
+            }
+
+            break;
+
+        case 6:
+            if(handle->button_level == handle->active_level) {
+                // 如果当前还按着，则等待LONG_WAIT_TICKS之后，进入HOLD状态
+                if(handle->ticks > LONG_WAIT_TICKS) {
+                    handle->event = (uint8_t)LONG_PRESS_HOLD;
+                    EVENT_CB(LONG_PRESS_HOLD);
+                    handle->ticks = 0;
+                    handle->state = 5;
+                }
+
+            } else { //releasd
+                // 未到达LONG_WAIT_TICKS就释放按键，回到空闲状态
+                handle->event = (uint8_t)PRESS_UP;
+                EVENT_CB(PRESS_UP);
+                handle->state = 0; //reset
+            }
+
+            break;
+    }
 }
 
 /**
@@ -154,16 +294,20 @@ void button_handler(struct Button* handle)
   * @param  handle: target handle strcut.
   * @retval 0: succeed. -1: already exist.
   */
-int button_start(struct Button* handle)
-{
-	struct Button* target = head_handle;
-	while(target) {
-		if(target == handle) return -1;	//already exist.
-		target = target->next;
-	}
-	handle->next = head_handle;
-	head_handle = handle;
-	return 0;
+int button_start(struct Button *handle) {
+    struct Button *target = head_handle;
+
+    while(target) {
+        if(target == handle) {
+            return -1;    //already exist.
+        }
+
+        target = target->next;
+    }
+
+    handle->next = head_handle;
+    head_handle = handle;
+    return 0;
 }
 
 /**
@@ -171,17 +315,19 @@ int button_start(struct Button* handle)
   * @param  handle: target handle strcut.
   * @retval None
   */
-void button_stop(struct Button* handle)
-{
-	struct Button** curr;
-	for(curr = &head_handle; *curr; ) {
-		struct Button* entry = *curr;
-		if (entry == handle) {
-			*curr = entry->next;
-//			free(entry);
-		} else
-			curr = &entry->next;
-	}
+void button_stop(struct Button *handle) {
+    struct Button **curr;
+
+    for(curr = &head_handle; *curr;) {
+        struct Button *entry = *curr;
+
+        if(entry == handle) {
+            *curr = entry->next;
+//          free(entry);
+        } else {
+            curr = &entry->next;
+        }
+    }
 }
 
 /**
@@ -189,11 +335,21 @@ void button_stop(struct Button* handle)
   * @param  None.
   * @retval None
   */
-void button_ticks()
-{
-	struct Button* target;
-	for(target=head_handle; target; target=target->next) {
-		button_handler(target);
-	}
+void button_ticks() {
+    struct Button *target;
+
+    for(target = head_handle; target; target = target->next) {
+        button_handler(target);
+    }
+
+    /* Check current keys. If has combine keys, it will be setted to combine mode */
+}
+
+void multi_button_init(void) {
+    memset(combine_button_id_list, COMBINE_BTN_ID_NONE, COMBINE_MAX_KEY * sizeof(combine_button_id_list[0]));
+    combine_counter = 0;
+    combine_callback = NULL;
+
+    head_handle = NULL;
 }
 
--- multi_button.h
+++ multi_button.h
@@ -2,60 +2,74 @@
  * Copyright (c) 2016 Zibin Zheng <znbin@qq.com>
  * All rights reserved
  */
- 
+
 #ifndef _MULTI_BUTTON_H_
 #define _MULTI_BUTTON_H_
 
 #include "stdint.h"
 #include "string.h"
 
+#define COMBINE_MAX_KEY         3                               // 最多允许组合键个数，按键超过限制后，新的按键将被无视
+#define COMBINE_BTN_ID_NONE     (~(0U))                        // 组合按键，无效按键表示
+
 //According to your need to modify the constants.
-#define TICKS_INTERVAL    5	//ms
-#define DEBOUNCE_TICKS    3	//MAX 8
-#define SHORT_TICKS       (300 /TICKS_INTERVAL)
-#define LONG_TICKS        (1000 /TICKS_INTERVAL)
+#define BTN_TICKS_INTERVAL      5 //ms                          // 定时周期，如果修改了，则定时扫描函数（button_ticks）的周期扫描时间需要同步调整
+#define DEBOUNCE_TICKS          3 //MAX 64                      // 去抖时间
+#define SHORT_TICKS             (50     / BTN_TICKS_INTERVAL)   // 触发短按的时间
+#define REPEAT_TICKS            (100    / BTN_TICKS_INTERVAL)   // 反复按键限定时间。在触发SHORT按键后，在该限定事件内再次按键，则进入反复按键模式。超时未再按键，则判定为松开
+#define LONG_TICKS              (1000   / BTN_TICKS_INTERVAL)   // 触发长按的时间
+#define LONG_WAIT_TICKS         (500    / BTN_TICKS_INTERVAL)   // 触发长按后，等待该时间后，才开始检测HOLD，避免长按结束立刻触发HOLD
+#define HOLD_PERIOD_TICKS       (200    / BTN_TICKS_INTERVAL)   // 保持长按时，每次上报处于长按状态的间隔时间
+#define COMBINE_TICKS           (5000   / BTN_TICKS_INTERVAL)   // 出现组合按键后，按超过该事件才会上报组合按键事件
 
-
-typedef void (*BtnCallback)(void*);
+typedef void (*BtnCallback)(void *);
 
 typedef enum {
-	PRESS_DOWN = 0,
-	PRESS_UP,
-	PRESS_REPEAT,
-	SINGLE_CLICK,
-	DOUBLE_CLICK,
-	LONG_RRESS_START,
-	LONG_PRESS_HOLD,
-	number_of_event,
-	NONE_PRESS
-}PressEvent;
+    PRESS_DOWN = 0,
+    PRESS_UP,
+    PRESS_REPEAT,
+    SINGLE_CLICK,
+    DOUBLE_CLICK,
+    LONG_RRESS_START,
+    LONG_PRESS_HOLD,
+    number_of_event,
+    COMBINE_PRESS,
+    NONE_PRESS
+} PressEvent;
 
 typedef struct Button {
-	uint16_t ticks;
-	uint8_t  repeat : 4;
-	uint8_t  event : 4;
-	uint8_t  state : 3;
-	uint8_t  debounce_cnt : 3; 
-	uint8_t  active_level : 1;
-	uint8_t  button_level : 1;
-	uint8_t  (*hal_button_Level)(void);
-	BtnCallback  cb[number_of_event];
-	struct Button* next;
-}Button;
-
-#ifdef __cplusplus  
-extern "C" {  
-#endif  
-
-void button_init(struct Button* handle, uint8_t(*pin_level)(), uint8_t active_level);
-void button_attach(struct Button* handle, PressEvent event, BtnCallback cb);
-PressEvent get_button_event(struct Button* handle);
-int  button_start(struct Button* handle);
-void button_stop(struct Button* handle);
-void button_ticks(void);
+    uint16_t    button_id;
+    uint16_t    ticks;
+    uint8_t     repeat;
+    uint8_t     event : 4;
+    uint8_t     state : 4;
+    uint8_t     debounce_cnt : 6;
+    uint8_t     active_level : 1;
+    uint8_t     button_level : 1;
+    uint8_t (*hal_button_Level)(void *btn);
+    BtnCallback  cb[number_of_event];
+    struct Button *next;
+} Button;
 
 #ifdef __cplusplus
-} 
+extern "C" {
+#endif
+
+void multi_button_init(void);
+
+void button_init(struct Button *handle, uint8_t(*pin_level)(void *btn), uint8_t active_level, uint16_t button_id);
+void button_attach(struct Button *handle, PressEvent event, BtnCallback cb);
+PressEvent get_button_event(struct Button *handle);
+uint8_t get_button_repeat_count(struct Button *handle);
+int  button_start(struct Button *handle);
+void button_stop(struct Button *handle);
+void button_ticks(void);
+
+uint8_t button_combine_check(uint16_t button_id);
+void button_combine_set_callback(BtnCallback cb);
+
+#ifdef __cplusplus
+}
 #endif
 
 #endif
